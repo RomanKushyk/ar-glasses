dd6759e288b020515deb94fe1f1bfb43f78d33fa
Roman Kushyk <romankushyk0@gmail.com>
2022-05-23 12:33:45 +0300

    added snapshot_options for glasses_list, glasses_list in separated file

--- src/utils/getPngFromFbx.ts
+++ src/utils/getPngFromFbx.ts
@@ -1,21 +1,25 @@
 import * as THREE from 'three';
 import {FBXLoader} from 'three/examples/jsm/loaders/FBXLoader';
+import {glasses_list} from '../const/glasses';
 
-export const getPngFromFbx = (filePath: string, position: number[], scale: number[]) => {
+type Glasses = typeof glasses_list[1];
+
+
+export const getPngFromFbx = (glasses: Glasses) => {
   return new Promise(async (resolve: any, reject: any) => {
     const strMime = 'image/png';
     const sizes = {
       width: 640,
-      height: 260,
+      height: 640,
     };
 
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(
-        18,
+        45,
         sizes.width/sizes.height,
         0.1,
-        37,
+        1000,
     );
     camera.position.set(0, 14, 40);
     scene.add(camera);
@@ -26,23 +30,30 @@ export const getPngFromFbx = (filePath: string, position: number[], scale: numbe
       preserveDrawingBuffer: true,
     });
     renderer.setSize(sizes.width, sizes.height);
+    // renderer.setClearColor(0x808080, 0.5);
     document.body.appendChild(renderer.domElement);
     const canvas = renderer.domElement;
-    document.body.append(renderer.domElement);
+    document.body.append(renderer.domElement); // !!!
 
     const fbxLoader = new FBXLoader();
-    let object = await fbxLoader.loadAsync(filePath);
+    let object = await fbxLoader.loadAsync(glasses.file_path);
 
-    object.position.set(...position);
-    object.scale.set(...scale);
-    object.rotation.set(4.8, 0, 0);
+    object.position.set(...glasses.snapshot_options.position);
+    object.scale.set(...glasses.snapshot_options.scale);
+    object.rotation.set(...glasses.snapshot_options.rotation);
+    object.getObjectByName(glasses.snapshot_options.bracketsName)
+        .traverse((obj) => {
+          if (obj.visible) {
+            obj.visible = false;
+          }
+        });
+    console.log(object);
     scene.add(object);
 
     renderer.render(scene, camera);
 
     requestAnimationFrame(() => {
       resolve(canvas.toDataURL(strMime));
-      console.log(canvas);
     });
   });
 };
