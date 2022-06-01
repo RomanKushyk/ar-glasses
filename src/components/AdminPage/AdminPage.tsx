ef8cbdcfbbb2c6a99d048397053abb427780ed20
Roman Kushyk <romankushyk0@gmail.com>
2022-06-01 18:28:17 +0300

    APIs for glassesList: get, add, edit delete. They are used in store

--- src/components/AdminPage/AdminPage.tsx
+++ src/components/AdminPage/AdminPage.tsx
@@ -11,6 +11,7 @@ import {SignOut} from '../SignOut';
 
 export const AdminPage: FC = () => {
   const [user] = useAuthState(firebaseAuth);
+
   return (
     <StoreContext.Provider value={store}>
       <div className="admin-page">
