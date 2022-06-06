784d3d39d399015f88902784fa2e8afaa0a223dc
Roman Kushyk <romankushyk0@gmail.com>
2022-06-06 18:50:48 +0300

    protected route, EditGlasses comp

--- src/components/EditGlasses/EditGlasses.tsx
+++ src/components/EditGlasses/EditGlasses.tsx
@@ -63,6 +63,44 @@ export const EditGlasses: FC = () => {
 
       <div className="edit-glasses__scene">
       </div>
+
+      <div className="edit-glasses__params-container params-container">
+        <div className="params-container__params-list">
+          list
+        </div>
+
+        <div className="params-container__navigation-panel">
+          <button
+            className="params-container__button params-container__button_scale"
+            type="button"
+            title="Scale"
+            // onClick={}
+          />
+
+          <button
+            className="params-container__button params-container__button_position"
+            type="button"
+            title="Position"
+            // onClick={}
+          />
+
+          <button
+            className="params-container__button params-container__button_rotate"
+            type="button"
+            title="Rotate"
+            // onClick={}
+          />
+
+          <button
+            className="params-container__button params-container__button_three"
+            type="button"
+            title="Three"
+            // onClick={}
+          />
+
+          <div className="params-container__indicator"/>
+        </div>
+      </div>
     </section>
   );
 };
