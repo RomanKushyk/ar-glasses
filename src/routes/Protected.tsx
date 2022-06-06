51d6cc3b4579638cdbbf25e59168c8e41534b359
Roman Kushyk <romankushyk0@gmail.com>
2022-06-06 18:50:48 +0300

    protected route, EditGlasses comp

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { FC } from 'react';
 import { firebaseAuth } from '../utils/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
@@ -6,6 +6,7 @@ import { SignIn } from '../components/SignIn';
 
 export const Protected: FC = () => {
   const [user] = useAuthState(firebaseAuth);
+  const location = useLocation();
 
   return !!user ? (
     <Outlet />
