39226dc2ee4c4f1dae94b01ee7b424fcbedf14da
Roman Kushyk <romankushyk0@gmail.com>
2022-06-07 16:27:39 +0300

    class EditGlassesOption

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -7,6 +7,8 @@ import { SignIn } from '../components/SignIn';
 export const Protected: FC = () => {
   const [user] = useAuthState(firebaseAuth);
   const location = useLocation();
+  console.log('user', user, !!user);
+  console.log('nav', location);
 
   return !!user ? (
     <Outlet />
