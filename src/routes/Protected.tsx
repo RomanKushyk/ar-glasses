1ddafbf7fecd872dd163e86f962a513ba8214c0a
Roman Kushyk <romankushyk0@gmail.com>
2022-06-09 18:04:20 +0300

    edit glasses comp: fixed many bugs, all inputs work, save & rename work.

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -6,9 +6,6 @@ import { SignIn } from '../components/SignIn';
 
 export const Protected: FC = () => {
   const [user] = useAuthState(firebaseAuth);
-  const location = useLocation();
-  console.log('user', user, !!user);
-  console.log('nav', location);
 
   return !!user ? (
     <Outlet />
