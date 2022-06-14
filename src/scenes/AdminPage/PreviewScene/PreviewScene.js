c112e308168cff79b2c30a0ecc7f9438249086ca
Roman Kushyk <romankushyk0@gmail.com>
2022-06-14 12:04:35 +0300

    refactor, optimization

--- src/scenes/AdminPage/PreviewScene/PreviewScene.js
+++ src/scenes/AdminPage/PreviewScene/PreviewScene.js
@@ -24,6 +24,7 @@ export class PreviewScene {
     );
     this.camera.position.set(0, 14, 40);
     this.scene.add(this.camera);
+
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       alpha: true,
