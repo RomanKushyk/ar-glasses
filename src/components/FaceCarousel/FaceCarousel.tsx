eb137087ba7703dc593a737868d4a427f5e1877d
Roman Kushyk <romankushyk0@gmail.com>
2022-06-23 12:10:55 +0300

    updated FaceCarousel

--- src/components/FaceCarousel/FaceCarousel.tsx
+++ src/components/FaceCarousel/FaceCarousel.tsx
@@ -1,40 +1,34 @@
-import {Carousel} from 'react-responsive-carousel';
 import "./face-carousel.scss";
 
+import { Carousel } from 'react-responsive-carousel';
+import {faceTypes} from '../../consts/faceTypes';
+
+const carouselConfig = {
+  showArrows: true,
+  showStatus: false,
+  showIndicators: true,
+  infiniteLoop: false,
+  showThumbs: false,
+  useKeyboardArrows: true,
+  autoPlay: false,
+  stopOnHover: true,
+  swipeable: true,
+  dynamicHeight: false,
+  emulateTouch: true,
+  autoFocus: false,
+  thumbWidth: 40,
+  selectedItem: 0,
+  interval: 2000,
+  transitionTime: 500,
+  swipeScrollTolerance: 5,
+  ariaLabel: undefined,
+};
+
 export const FaceCarousel = () => {
-  const config = {
-    showArrows: true,
-    showStatus: false,
-    showIndicators: true,
-    infiniteLoop: false,
-    showThumbs: false,
-    useKeyboardArrows: true,
-    autoPlay: false,
-    stopOnHover: true,
-    swipeable: true,
-    dynamicHeight: false,
-    emulateTouch: true,
-    autoFocus: false,
-    thumbWidth: 40,
-    selectedItem: 0,
-    interval: 2000,
-    transitionTime: 500,
-    swipeScrollTolerance: 5,
-    ariaLabel: undefined,
-  }
-
-  const items = [
-    {id: 1, title: 'item #1'},
-    {id: 2, title: 'item #2'},
-    {id: 3, title: 'item #3'},
-    {id: 4, title: 'item #4'},
-    {id: 5, title: 'item #5'}
-  ];
-
   return (
     <div className="face-carousel">
-      <Carousel {...config}>
-        {items.map(item => (
+      <Carousel {...carouselConfig}>
+        {faceTypes.map(item => (
           <div
             key={item.id}
             className="face-carousel__image-container"
@@ -42,7 +36,8 @@ export const FaceCarousel = () => {
             <img
               className="face-carousel__image"
               alt=""
-              src="https://i0.wp.com/post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/03/GettyImages-1092658864_hero-1024x575.jpg?w=1155&h=1528"/>
+              src={document.location.origin + '/' + item.src}
+            />
           </div>
         ))}
       </Carousel>
---
+++ src/consts/faceTypes.ts
@@ -0,0 +1,28 @@
+interface FaceTypes {
+  id: number,
+  name: string,
+  src: string,
+}
+
+export const faceTypes: FaceTypes[] = [
+  {
+    id: 0,
+    name: 'test',
+    src: 'assets/FaceTypes/test.webp',
+  },
+  {
+    id: 1,
+    name: 'test1',
+    src: 'assets/FaceTypes/test.webp',
+  },
+  {
+    id: 2,
+    name: 'test2',
+    src: 'assets/FaceTypes/test.webp',
+  },
+  {
+    id: 3,
+    name: 'test2',
+    src: 'assets/FaceTypes/test.webp',
+  },
+];
